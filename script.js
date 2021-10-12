5554504ff8572ad5398f13f72e7c7d7f0a538027
“TorrLaura” <laplebe35@gmail.com>
2021-10-11 20:13:39 -0500

    completed LoopsExercise

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1
+for (i= 1; i < 8; i ++) {
+    console.log(i);
+}
+
+for (i = 5; i <= 25; i += 4 ){
+    console.log(i);
+}
+
+const wizards = [
+    'Harry Potter',
+    'Hermione Granger',
+    'Ron Weasley'
+    
+];
+for(i = 0; i < wizards.length; i++){
+    console.log(wizards[i]);
+}
+
+let 
